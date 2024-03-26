93921fadadb6331e2416c37b28450d0a896d46fb
Ricardo <rick_ferry1@hotmail.com>
2024-03-25 21:59:17 -0300

    starter

---
+++ src/test/java/com/ferry/myifood/MyifoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferry.myifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyifoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
