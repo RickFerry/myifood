94d435939fe1e66b2896a0d4a04a9d46c8fcea06
Ricardo <rick_ferry1@hotmail.com>
2024-04-07 23:48:45 -0300

    CRUD completo para recurso de estado

--- src/main/java/com/ferry/myifood/domain/controller/EstadoController.java
+++ src/main/java/com/ferry/myifood/domain/controller/EstadoController.java
@@ -4,9 +4,7 @@ import com.ferry.myifood.domain.model.Estado;
 import com.ferry.myifood.domain.service.EstadoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,38 @@ public class EstadoController {
     @GetMapping
     public ResponseEntity<List<Estado>> listar() {
         return ResponseEntity.ok(estadoService.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(estadoService.buscar(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Estado> salvar(@RequestBody Estado estado) {
+        return ResponseEntity.ok(estadoService.salvar(estado));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estado> atualizar(@PathVariable Long id, @RequestBody Estado estado) {
+        try {
+            return ResponseEntity.ok(estadoService.atualizar(id, estado));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        try {
+            estadoService.deletar(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/ferry/myifood/domain/service/EstadoService.java
+++ src/main/java/com/ferry/myifood/domain/service/EstadoService.java
@@ -3,8 +3,11 @@ package com.ferry.myifood.domain.service;
 import com.ferry.myifood.domain.model.Estado;
 import com.ferry.myifood.domain.repository.EstadoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -14,5 +17,33 @@ public class EstadoService {
 
     public List<Estado> listar() {
         return estadoRepository.findAll();
+    }
+
+    public Estado buscar(Long id) {
+        return estadoRepository.findById(id).orElseThrow(() -> new RuntimeException("Estado não encontrado"));
+    }
+
+    @Transactional
+    public Estado salvar(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    @Transactional
+    public Estado atualizar(Long id, Estado estado) {
+        return estadoRepository.findById(id)
+                .map(e -> {
+                    BeanUtils.copyProperties(estado, e, "id");
+                    return estadoRepository.save(e);
+                })
+                .orElseThrow(() -> new RuntimeException("Não existe um estado com o id informado"));
+    }
+
+    @Transactional
+    public void deletar(Long id) {
+        estadoRepository.findById(id)
+                .ifPresentOrElse(estadoRepository::delete,
+                        () -> {
+                            throw new EntityNotFoundException("Não existe um estado com o id informado");
+                        });
     }
 }
