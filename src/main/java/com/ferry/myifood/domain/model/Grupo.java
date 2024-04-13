bfd66c3b8dd73e64dde5ef81e113c29c92a77257
Ricardo <rick_ferry1@hotmail.com>
2024-04-13 12:44:11 -0300

    Adicionando entidades de Grupo e Usuario

--- src/main/java/com/ferry/myifood/domain/model/Grupo.java
+++ src/main/java/com/ferry/myifood/domain/model/Grupo.java
@@ -1,39 +1,39 @@
 package com.ferry.myifood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Produto {
+public class Grupo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String descricao;
-    private BigDecimal preco;
-    private Boolean ativo;
 
-    @ManyToOne
-    @JsonIgnore
-    private Restaurante restaurante;
+    @ManyToMany
+    @JoinTable(name = "Grupo_permissoes",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissoes_id"))
+    private Set<Permissao> permissoes = new LinkedHashSet<>();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Produto produto = (Produto) o;
-        return Objects.equals(id, produto.id);
+        Grupo grupo = (Grupo) o;
+        return Objects.equals(id, grupo.id);
     }
 
     @Override
--- src/main/java/com/ferry/myifood/domain/model/Usuario.java
+++ src/main/java/com/ferry/myifood/domain/model/Usuario.java
@@ -1,39 +1,40 @@
 package com.ferry.myifood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Produto {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String descricao;
-    private BigDecimal preco;
-    private Boolean ativo;
+    private String email;
+    private String senha;
 
-    @ManyToOne
-    @JsonIgnore
-    private Restaurante restaurante;
+    @ManyToMany
+    @JoinTable(name = "Usuario_grupos",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupos_id"))
+    private Set<Grupo> grupoes = new LinkedHashSet<>();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Produto produto = (Produto) o;
-        return Objects.equals(id, produto.id);
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
     }
 
     @Override
