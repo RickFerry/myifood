404aa9d5e243c86dbd70bfc0ad8a39f27d14ca93
Ricardo <rick_ferry1@hotmail.com>
2024-04-14 13:26:32 -0300

    Correcao seed, criacao das entidades Pedido e ItemPedido, criacao enum de StatusPedido

---
+++ src/main/java/com/ferry/myifood/domain/model/ItemPedido.java
@@ -0,0 +1,34 @@
+package com.ferry.myifood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+}
---
+++ src/main/java/com/ferry/myifood/domain/model/enums/StatusPedido.java
@@ -0,0 +1,8 @@
+package com.ferry.myifood.domain.model.enums;
+
+public enum StatusPedido {
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+}
