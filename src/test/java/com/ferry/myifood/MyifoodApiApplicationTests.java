0fab0976da9e7c4c862bdb13ce7eb647a2133bd8
Ricardo <rick_ferry1@hotmail.com>
2024-05-01 13:10:01 -0300

    Ajuste!

--- src/test/java/com/ferry/myifood/MyifoodApiApplicationTests.java
+++ src/test/java/com/ferry/myifood/MyifoodApiApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MyifoodApiApplicationTests {
 
+    /**
+     * Test context loads.
+     */
     @Test
     public void contextLoads() {
         MyifoodApiApplication.main(new String[] {});
