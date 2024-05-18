0c598e95e59c610f43884bbfd28d2c7b68dffbc0
Ricardo <rick_ferry1@hotmail.com>
2024-05-17 22:54:26 -0300

    Refactor

--- src/main/java/com/ferry/myifood/domain/controller/RestauranteController.java
+++ src/main/java/com/ferry/myifood/domain/controller/RestauranteController.java
@@ -1,28 +1,18 @@
 package com.ferry.myifood.domain.controller;
 
-import javax.persistence.EntityNotFoundException;
-import javax.validation.Valid;
-
 import com.ferry.myifood.domain.model.dtos.input.RestauranteIN;
 import com.ferry.myifood.domain.model.dtos.output.RestauranteOUT;
 import com.ferry.myifood.domain.model.dtos.update.RestauranteUP;
+import com.ferry.myifood.domain.service.RestauranteService;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ferry.myifood.domain.model.dtos.RestauranteDto;
-import com.ferry.myifood.domain.service.RestauranteService;
-
-import lombok.AllArgsConstructor;
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
